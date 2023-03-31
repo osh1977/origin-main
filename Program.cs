4c8af449c9640526b907060c033cdf4c3c77eb40
Oleg <osh1977@yandex.ru>
2023-03-31 22:37:38 +0300

    Найти все четные

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("ввести число N");
+int N=Convert.ToInt32(Console.ReadLine());
+string result = N>2 ? "" : "облом";
+for (int i = 2; i <= N; i=i+2){
+    
+    result+=i+",";
+}
+System.Console.WriteLine(result);
